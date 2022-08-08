8193e4bd2180f96b1ca824d34cb6c57cd3734663
NosaR <74286379+NosaR@users.noreply.github.com>
2022-08-08 15:34:07 +0700

    Add files via upload

---
+++ views/contact.blade.php
@@ -0,0 +1,7 @@
+@extends('layout')
+
+@section('content')
+
+<p>Ini Adalah Halaman Contact</p>
+
+@endsection
---
+++ views/home.blade.php
@@ -0,0 +1,23 @@
+@extends('layout')
+
+@section('content')
+
+<div class="article-data">
+    <a href="#"><h3 class="article-title">Ini Adalah Judul Artikel</h3></a>
+    <small>20/08/2022 Author : Jhon Doe</small>
+    <p class="article-body">Lorem ipsum dolor sit amet consectetur.</p>
+</div>
+
+<div class="article-data">
+    <a href="#"><h3 class="article-title">Ini Adalah Judul Artikel</h3></a>
+    <small>20/08/2022 Author : Jhon Doe</small>
+    <p class="article-body">Lorem ipsum dolor sit amet consectetur.</p>
+</div>
+
+<div class="article-data">
+    <a href="#"><h3 class="article-title">Ini Adalah Judul Artikel</h3></a>
+    <small>20/08/2022 Author : Jhon Doe</small>
+    <p class="article-body">Lorem ipsum dolor sit amet consectetur.</p>
+</div>
+
+@endsection
